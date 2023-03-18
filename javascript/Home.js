86089b2a8f8ce7d638d87bade0c477c00357c4a1
EvaCalc <128114583+EvaCalc@users.noreply.github.com>
2023-03-17 19:04:01 -0500

    Update Home.js

--- javascript/Home.js
+++ javascript/Home.js
@@ -1,1 +1,14 @@
+     const delay = ms => new Promise(res => setTimeout(res, ms));
+	  window.onload = function() {
+ 	  OnStart();
+	  };
+   function OnStart(){ 
+yourFunction()
+   }
+const yourFunction = async () => {
+  await delay(5000);
+  alert("hi");
 
+  await delay(5000);
+  console.log("Waited an additional 5s");
+};
