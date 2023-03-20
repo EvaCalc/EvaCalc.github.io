671b61ced9634f8d9581df427f94fa9dae9f0ed1
EvaCalc <128114583+EvaCalc@users.noreply.github.com>
2023-03-20 08:46:08 -0500

    Update Home.js

--- javascript/Home.js
+++ javascript/Home.js
@@ -1,24 +1,26 @@
-     const delay = ms => new Promise(res => setTimeout(res, ms));
-	  window.onload = function() {
-		  document.getElementById("Main").style.opacity="0";
- 	  OnStart();
-	  };
-   function OnStart(){ 
+const delay = ms => new Promise(res => setTimeout(res, ms));
+window.onload = function() {
+	document.getElementById("Main").style.opacity="0";
+ OnStart();
+};
+function OnStart(){ 
 yourFunction();
-	   interval();
-   }
+ interval();
+}
 const yourFunction = async () => {
-  await delay(5000);
+await delay(5000);
 document.getElementById("Main").style.opacity="1";
-	document.getElementById("loader").style.opacity="0";
+document.getElementById("loader").style.opacity="0";
 };
 const interval = setInterval(function() {
-   var w = window.innerWidth;
-	if(w < 1024){
-		document.getElementById("Header").style.fontSize="xx-large";
-	}
-	if(w < 804){
-	document.getElementById("Header").style.fontSize="x-large";
-	}
-	console.log(w.toString())
- }, 100);
+var w = window.innerWidth;
+if(w < 1024){
+  document.getElementById("Header").style.fontSize="xx-large";
+}
+if(w < 804){
+document.getElementById("Header").style.fontSize="x-large";
+document.getElementById("Header").style.left="75%";
+document.getElementById("butons").style.left="62%";
+}
+console.log(w.toString())
+}, 100);
